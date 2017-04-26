60238dc7a40d881de04bb6ca41369caadc78f336
Tom Elliott <tom.elliott@nyu.edu>
2017-04-26 13:51:52 -0500

    add diacritic sorting to ensure repeatability in equivalence; tweak error message; adjust some variable names for sense

--- textnorm/__init__.py
+++ textnorm/__init__.py
@@ -10,7 +10,6 @@ __copyright__ = 'Copyright ©️ 2017 New York University'
 __license__ = 'See LICENSE.txt'
 __version__ = '0.3'
 
-import re
 import unicodedata
 
 
@@ -64,20 +63,68 @@ def normalize_unicode(v: str, target='NFC', check_compatible=False):
     This function wraps unicodedata.normalize from the standard library,
     adding the optional compatibility check when appropriate.
     """
-    canonical = unicodedata.normalize(target, v)
+    normalized = unicodedata.normalize(target, v)
+    normalized = _sort_diacritics(normalized)
     if check_compatible:
         if target == 'NFC':
             compatibility_target = 'NFKC'
         elif target == 'NFD':
             compatibility_target = 'NFKD'
+        elif target == 'NFKC':
+            compatibility_target = 'NFC'
+        elif target == 'NFKD':
+            compatibility_target = 'NFD'
         compatible = unicodedata.normalize(compatibility_target, v)
-        if canonical != compatible:
+        if normalized != compatible:
             msg = (
                 'Unicode normalization may have changed the string "{}" in '
-                'an undesireable way. The canonical composition form ({}: '
-                '"{}") does not match the compatibility composition form ('
-                '{}: "{}").'
+                'an undesireable way or may have failed to do so in a manner '
+                'desired. The {} normalized form '
+                '"{}" ({}) does not match the corresponding {} form '
+                '"{}" ({}).'
                 ''.format(
-                    v, target, canonical, compatibility_target, compatible))
+                    v,
+                    target,
+                    normalized,
+                    normalized.encode('ascii', 'namereplace'),
+                    compatibility_target,
+                    compatible,
+                    compatible.encode('ascii', 'namereplace')))
             raise ValueError(msg)
-    return canonical
+    return normalized
+
+
+def _sort_diacritics(v: str):
+    """Ensure combining diacritics are always ordered the same way.
+
+    It appears that unicodedata.normalize does not enforce a canonical
+    ordering of combining diacritics in NFD and NFKD when the string
+    being normalized contains combining diacritics in arbitrary order and
+    the 'Canonical Combining Class' of two or more of those diacritics has
+    the same value. This function sorts all combining diacriticals that
+    follow a character with combining class == 0 first according to their
+    combining classes and then according to their code point value. This
+    ensures a repeatable sequence, regardless of the arbitrary order in
+    which the combining diacritics in the input string occur.
+    """
+
+    def postfix(v: list):
+        if len(v) > 0:
+            return ''.join(
+                sorted(
+                    v,
+                    key=lambda x: (unicodedata.combining(x), ord(x))))
+        else:
+            return ''
+
+    result = ''
+    diacritics = []
+    for c in v:
+        if unicodedata.combining(c) == 0:
+            result += postfix(diacritics)  # diacritics for preceding character
+            diacritics = []
+            result += c  # now the current non-combining character
+        else:
+            diacritics.append(c)
+    result += postfix(diacritics)  # final diacritics
+    return result
